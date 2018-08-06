3f0cf791144b6df93dba16a9a5aa9d404729e2c0
fred09 <fred_15@outlook.com>
2018-08-06 22:01:13 +0200

    Mejorando la plantilla html y modificando archivo enviar para envio de emails

--- phpmailer1/enviar.php
+++ phpmailer1/enviar.php
@@ -4,6 +4,110 @@ require 'class.phpmailer.php';
 require 'class.smtp.php';
 
 $link ="https://www.youtube.com";
+
+$html =  '
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+</head>
+<body>
+	<style type="text/css">
+		body{
+			margin: 0;
+			padding: 0;
+			font-family: sans-serif;
+			font-size: 0.7em;
+
+		}
+		 img{
+			padding: 20px;
+		}
+
+		.text-main{
+			border-top: 1px solid #FF5733;
+			border-bottom:1px solid #FF5733;
+			margin: 5px;
+			padding: 22px 5px;
+			font-size: 1.3em;
+		}
+		.text-main a{
+			text-decoration: none;
+			color: #FF5733;
+		}
+
+		.enlace{
+			font-size: 1.6rem;
+			margin-top: 50px;
+		}
+
+		.colortext{
+			color:#FF5733;
+		}
+
+		.colorblue{
+			color: #318EF9;
+		}
+
+		.colorfa{
+			color:#FF5733;
+			margin-right: 2px;
+			font-size: 1.5em;
+		}
+
+		.footer p,h2 {
+			padding: 0px 12px;
+		}
+
+		.about{
+			color: #67CA4A;
+		}
+
+		.margin-top{
+			margin-top: 20px;
+		}
+	</style>
+	<div class="main">
+		<div class="cabecera">
+			<!--<img src="logo_cmas.png" alt="logo_cmas">-->
+			<img src="cid:logo_cmas">
+		</div>
+		<div class="text-main">
+			<h1>Hola Usuario!</h1>
+			<p>¡Gracias por ser parte del Equipo C+ Club! 
+				<br><br>
+				Para completar tu registro pulsa el siguiente enlace:
+			</p>
+			<BR>
+			<a href="#" class="enlace">CONFIRMA TU REGISTRO AQUÍ</a>
+			
+			<p class="margin-top">Si no has solicitado un envio de correo de C+ Club hagas caso omiso de este correo <br>
+				Un Saludo. <br><br>
+				El equipo C+ Club.
+			</p>
+		</div>
+		<div class="footer">
+			<h2 class="colorfa">Contacto</h2>
+			
+			<p class="colorfa">Av. Río Churubusco 583 esq. La viga <br>
+				Col. Sector Popular, 06090<br>
+				T. Churubusco 5670 8281<br>
+				T. Narvarte 2881 5995<br>
+				Email. <a href="#" class="colorblue">email@email.com</a> <br>
+				<a href="http://www.cmasclub.com/" class="colortext">www.cmasclub.com</a></p>
+		
+			<P class="about margin-top">La información contenida en este correo electrónico es Información Confidencial. Está prohibida la divulgación, difusión y/o distribución,  reproducción y duplicación parcial o total sin la previa autorización por escrito de Club C+ Uno S.A.P.I. de C.V. 
+			La información Confidencial deberá ser utilizada únicamente para el fin con la que fue proporcionada. Por lo tanto, el receptor de la Información Confidencial se hace responsable de los daños y perjuicios que pudiera ocasionar el incumplimiento a las obligaciones antes establecidas.
+			Club C+ Uno S.A.P.I. de C.V., es responsable del tratamiento de sus datos personales, con domicilio en Av. Río Churubusco 583, Local AN4, Colonia Sector Popular, C.P. 09060, Delegación Iztapalapa, México Distrito Federal.
+			Los datos personales aquí recabados serán utilizados para proveer los servicios que ha solicitado y para dar cumplimiento a las obligaciones que hemos contraído con usted. Para mayor información acerca del tratamiento y de los derechos que puede hacer valer, solicitar el aviso de privacidad integral al  correo del cual se emitió la información, accediendo a nuestra página <a href="http://www.cmasclub.com/" class="colorblue">www.cmasclub.com</a>, solicitándola al correo datospersonales@cmasclub.com o en nuestro domicilio antes referido.
+			Si usted no es el destinatario, le pedimos destruya esta comunicación y agradeceremos nos lo haga saber al  correo del cual se emitió la información.</P>
+		</div>
+	</div>	
+</body>
+</html>
+';
+
 $phpmailer = new PHPMailer();
 
 $phpmailer->IsSMTP(true); // use SMTP
@@ -20,17 +124,15 @@ $phpmailer->Password = "Fredymetal912";
 
 $phpmailer->From = "Alfredo@outlook.com";
 $phpmailer->FromName = "Alfredo Vazquez";
-$phpmailer->addAddress('fred_15@outlook.com','Alfredo');
-$phpmailer->Subject ="Te a llegado este Mensaje";
-//$phpmailer->AddEmbeddedImage('logo_cmas.png');
-$phpmailer->Body = file_get_contents('template.html');
-
+$phpmailer->addAddress('alfredo.vazquez@meltsan.com','Alfredo');
+$phpmailer->Subject ="Seguimiento de Registro C+ Club";
+$phpmailer->AddEmbeddedImage(dirname(__FILE__).'/logo_cmas.png','logo_cmas');
+$phpmailer->Body = $html;
+$phpmailer->CharSet = 'UTF-8';
 
 $phpmailer->IsHTML(true);
 
-
-
-if(!$phpmailer->Send()){
+if($phpmailer->Send()){
 	echo "bien";
 }else{
 	echo "Error: " . $phpmailer->ErrorInfo;
